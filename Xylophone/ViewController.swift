85fb5f22e345fb7fd0bb4b2d967984f6f176be14
Konstantin <don.kamatoz@bk.ru>
2023-01-22 14:14:34 +0500

    Досмотрел модуль. Сделал все, включая челендж.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -10,7 +10,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound(soundName: sender.titleLabel?.text ?? "C")
+        playSound(soundName: sender.currentTitle ?? "C")
+        changeVisibility(target: sender)
     }
     
     func playSound(soundName: String) {
@@ -18,5 +19,11 @@ class ViewController: UIViewController {
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
                 
+    }
+    func changeVisibility(target button: UIButton) {
+        button.alpha = 0.6
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            button.alpha = 1.0
+        }
     }
 }
